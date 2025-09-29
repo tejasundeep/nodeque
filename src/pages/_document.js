17d18f97ffcf8914e5abc3eb674b262648f8ec64
tejasundeep <tsreddykarri@gmail.com>
2025-09-29 21:30:37 +0530

    Added bootstrap

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
       </body>
     </Html>
   );
